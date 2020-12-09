591c5ba9f34eed2916c0a70025d68d4f2bc59256
ezequiel-7 <ezekrom007@gmail.com>
2020-12-09 11:20:26 -0300

    feature/logicajavascript

---
+++ logicajavascript/prompt.js
@@ -0,0 +1,22 @@
+alert('Obrigatório o Preenchimento da Ficha')
+var clientes = [];
+var cliente = {}
+
+function cadastrarCliente(cliente) {
+clientes.push(cliente);
+}
+
+function preencherCliente() {
+cliente.nome = prompt("Digite seu Nome");
+cliente.nacimento = prompt("Digite sua Data de Nacimento");
+cliente.cpf = prompt("Digite seu cpf");
+cliente.rg = prompt("Digite seu rg");
+cliente.endereco = prompt("Digite seu Endereço");
+}
+
+preencherCliente();
+cadastrarCliente(cliente);
+console.log(clientes);
+
+let result = confirm('Tem certeza que quer confirmar isso?');
+console.log(result);
